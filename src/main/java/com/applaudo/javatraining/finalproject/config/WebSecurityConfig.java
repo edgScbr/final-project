de16dcfa62824f08ba2988f717be75bd787a2702
edgScbr <edgardo.scb@gmail.com>
2023-04-19 16:46:45 -0600

    add security config

---
+++ src/main/java/com/applaudo/javatraining/finalproject/config/WebSecurityConfig.java
@@ -0,0 +1,35 @@
+package com.applaudo.javatraining.finalproject.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    public static final String USER = "user";
+    private final JwtAuthConverter jwtAuthConverter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers(HttpMethod.GET, "/actuator/health", "actuator/health/**").permitAll()
+                .requestMatchers(HttpMethod.GET, "/store").hasRole(USER)
+                .requestMatchers(HttpMethod.GET, "/actuator/metrics/", "/actuator/metrics/**").hasRole(USER)
+                .requestMatchers(HttpMethod.GET, "/actuator/loggers/", "/actuator/loggers/**").hasRole(USER)
+                .anyRequest().authenticated();
+        http.oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthConverter);
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        return http.build();
+    }
+
+}
