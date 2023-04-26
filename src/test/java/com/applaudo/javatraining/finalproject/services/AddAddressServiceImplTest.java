b65562e32cbe4faf3141bdf8881865533e2e6a61
edgScbr <edgardo.scb@gmail.com>
2023-04-25 21:51:46 -0600

    add add address service impl test

---
+++ src/test/java/com/applaudo/javatraining/finalproject/services/AddAddressServiceImplTest.java
@@ -0,0 +1,83 @@
+package com.applaudo.javatraining.finalproject.services;
+
+import com.applaudo.javatraining.finalproject.TestUtilities;
+import com.applaudo.javatraining.finalproject.controllers.responses.OrderResponse;
+import com.applaudo.javatraining.finalproject.mappers.OrderMapper;
+import com.applaudo.javatraining.finalproject.models.Customer;
+import com.applaudo.javatraining.finalproject.models.Item;
+import com.applaudo.javatraining.finalproject.models.Order;
+import com.applaudo.javatraining.finalproject.repositories.OrderRepository;
+import com.applaudo.javatraining.finalproject.services.interfaces.AddAddressService;
+import com.applaudo.javatraining.finalproject.services.interfaces.UtilityService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.HashSet;
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+
+public class AddAddressServiceImplTest extends TestUtilities {
+
+    AddAddressService addAddressService;
+
+
+    UtilityService utilityService = mock(UtilityService.class);
+    OrderMapper orderMapper = mock(OrderMapper.class);
+    OrderRepository orderRepository = mock(OrderRepository.class);
+
+    @BeforeEach
+    void init() {
+        addAddressService = new AddAddressServiceImpl(
+                this.utilityService,
+                this.orderMapper,
+                this.orderRepository);
+    }
+
+    @Test
+    public void givenAddressRequest_whenAddAddress_thenReturnOrderResponse() throws Exception {
+        given(utilityService.getOrderByUserNameAndStatus(anyString(), any())).willReturn(Optional.of(orderModel));
+        given(orderRepository.save(any())).willReturn(orderModel);
+        given(orderMapper.orderToOrderResponse(any())).willReturn(orderResponse);
+
+        OrderResponse response = addAddressService.addAddressToOrder(customerModel.getUserName(), addressRequest);
+
+        assertThat(response).isNotNull();
+        assertThat(response.getDeliveryAddress()).isSameAs(addressResponse);
+    }
+
+    @Test
+    public void givenInvalidAddress_whenAddAddress_thenReturnException() throws Exception {
+        Customer withNoAddress = customerModel;
+        withNoAddress.setAddresses(new HashSet<>());
+        Order order = orderModel;
+        order.setCustomer(withNoAddress);
+
+        given(utilityService.getOrderByUserNameAndStatus(anyString(), any())).willReturn(Optional.of(order));
+
+        Assertions.assertThrows(ResponseStatusException.class,
+                () -> {
+                    OrderResponse response = addAddressService
+                            .addAddressToOrder(withNoAddress.getUserName(), addressRequest);
+                });
+    }
+
+    @Test
+    public void givenNoCheckout_whenAddAddress_thenReturnException() throws Exception {
+        given(utilityService.getOrderByUserNameAndStatus(anyString(), any())).willReturn(Optional.empty());
+
+        Assertions.assertThrows(ResponseStatusException.class,
+                () -> {
+                    OrderResponse response = addAddressService
+                            .addAddressToOrder(customerModel.getUserName(), addressRequest);
+                });
+    }
+}
